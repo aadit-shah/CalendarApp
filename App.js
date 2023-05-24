d7020c8a30b241aa6863944fade579f134dbd09f
aadit-shah <aaditms247@gmail.com>
2023-05-24 17:03:08 -0500

    Placeholder for calendar screen

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from 'react-native';
 import StartingScreen from './app/screens/StartingScreen';
+import CalendarScreen from './app/screens/CalendarScreen';
 
 
 
 export default function App() {
   
   return (
-    <StartingScreen/>
+    <CalendarScreen/>
   );
 }
 
